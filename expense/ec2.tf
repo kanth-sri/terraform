f6ad64c0dbe19fbc5ac8392a78d64bc871d2a171
kanth-sri <srikanthadepu8008@gmail.com>
2024-09-13 21:25:09 +0530

    create infra for expense project

---
+++ expense/ec2.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "Allow SSH inbound traffic and all outbound traffic"
+  vpc_id      = "vpc-0a2339349dedaa1b5"
+  
+  egress { //allow traffic from every protocol and from every port.
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress { //allow traffic from tcp protocol  and from 22 port and from all ip range
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp" 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress { 
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp" 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress { 
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp" 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  tags = {
+    Name = "allow_tcp"
+  }
+}
+
+resource "aws_instance" "expense" {
+
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = var.environment == "prod" ? "t3.small" : "t3.micro"  //condition to check weather its prod or dev 
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  count = length(var.instance_names)
+
+  tags =  merge(var.common_tags, #merging two maps 
+    {
+       Name = var.instance_names[count.index]
+    })
+}
