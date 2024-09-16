a8ab80ac220548978206354e865d7923647f659b
kanth-sri <srikanthadepu8008@gmail.com>
2024-09-16 22:38:31 +0530

    tf for-each loop

---
+++ for-each/ec2.tf
@@ -0,0 +1,51 @@
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "Allow SSH inbound traffic and all outbound traffic"
+  vpc_id      = "vpc-0a2339349dedaa1b5"
+  
+  egress { //allow traffic from every protocol and from every port.
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress { //allow traffic from tcp protocol  and from 22 port and from all ip range
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp" 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress { 
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp" 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress { 
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp" 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  tags = {
+    Name = "allow_tcp"
+  }
+}
+
+resource "aws_instance" "expense" {
+
+  ami =  "ami-09c813fb71547fc4f"
+  for_each = var.instance_names
+  instance_type = each.value
+  //instance_type = var.environment == "prod" ? "t3.small" : "t3.micro"  //condition to check weather its prod or dev 
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  tags = merge(var.common_tags,
+   {Name = each.key}
+   )
+}
---
+++ for-each/vars.tf
@@ -0,0 +1,33 @@
+variable "common_tags" {
+    type = map
+    default = {
+        project = "expense"
+        env = "dev"
+    }
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+# variable "instance_names" {
+#     type = list(string)
+#     default = [ "mysql", "backend", "frontend" ]
+# }
+variable "instance_names" {
+    type = map(string)
+    default = {
+     "mysql" = "t3.small"
+     "frontend" = "t3.micro"
+     "backend" = "t3.micro"
+    }
+  
+}
+
+variable "zone_id" {
+    default = "Z02632882WFOI23E22TF"
+}
+
+variable "domain" {
+  default = "srikanthadepu.online"
+}
